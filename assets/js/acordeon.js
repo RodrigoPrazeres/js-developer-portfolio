36d4a353a28091275f7486847540b8e1bc946584
RodrigoPrazeres <rodrigosoaresprazeres@gmail.com>
2024-06-28 23:59:29 -0300

    atualizando código

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,14 +1,19 @@
-
+// Select all elements with the class '.acordeon .trigger'
 const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
 
+// Iterate over each found trigger
 acordeonTriggers.forEach((trigger) => {
+
+    // Add an event listener for the click event
     trigger.addEventListener('click', (e) => {
         const acordeon = trigger.parentElement
         const isOpen = acordeon.classList.contains('open')
 
+        // If the acordeon is open, remove the 'open' class to close it - If it's not open, add the 'open' class to open it
         if (isOpen) {
             acordeon.classList.remove('open')
-        } else {
+        }
+        else {
             acordeon.classList.add('open')
         }
     })
