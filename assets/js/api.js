6057a49cace278c59857193e91e86e038b89d28a
RodrigoPrazeres <rodrigosoaresprazeres@gmail.com>
2024-06-29 00:37:18 -0300

    atualizações

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,7 @@
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/digitalinnovationone/js-developer-portfolio/main/data/profile.json'
-    const fetching = fetch(url)
 
-    return await fetching.json()
+async function fetchProfileData() {
+    const url = 'https://raw.githubusercontent.com/RodrigoPrazeres/js-developer-portfolio/main/data/profile.json';
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
